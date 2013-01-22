ee760f6018477ddd7ef4f6618b85bbb9d8a71a65
David Gianforte <david.gianforte@gmail.com>
2013-01-22 11:56:24 -0700

    loop and get all the tokens, printing them out

--- mp/src/MP.java
+++ mp/src/MP.java
@@ -4,20 +4,30 @@ import java.io.IOException;
 class MP {
 	MP(String filename) {
 		MPscanner scanner = new MPscanner();
+		Token token = null;
 		try {
 			scanner.openFile(filename);
-			Token token = scanner.getToken();
-			while(token != null) {
-				token = scanner.getToken();
-			}
+			token = scanner.getToken();
+
 		} catch (FileNotFoundException e) {
 			System.out.println("Error: File " + filename + " not found");
 			System.exit(1);
 		} catch (IOException ioe) {
-			System.out.println("Error while reading input file.");
+			System.out.println("Error while reading the first char of " + filename);
 			System.exit(1);
 		}
-		
+		while(token != null) {
+			System.out.println(pad(token.getToken().name(), 14) + " " +
+					pad("" + token.getLine(), 4) + " " +
+					pad("" + token.getColumn(), 3) + " " +
+					token.getLexeme());
+			try {
+				token = scanner.getToken();
+			} catch (IOException e) {
+				System.out.println("Error while reading " + filename);
+				System.exit(1);
+			}
+		}
 	}
 	
 	public static void main(String args[]) {
@@ -27,5 +37,9 @@ class MP {
 		} else {
 			new MP(args[0]);
 		}
+	}
+	
+	public static String pad(String s, int n) {
+		return String.format("%1$-" + n + "s", s);
 	}
 }
