0e52ac92ac1dddf85f500f9fbd4f09a0f23ca1f6
Jordan Bowman <spearmintrhino1@hotmail.com>
2013-01-22 11:42:11 -0700

    getToken returns Token now

--- mp/src/MPscanner.java
+++ mp/src/MPscanner.java
@@ -1,9 +1,13 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 class MPscanner {
-	BufferedReader reader;
+	private BufferedReader reader;
+	private int lineNumber = 1;
+	private int columnNumber = 1;
+	private Token token;
 	
 	MPscanner() {
 		
@@ -15,8 +19,17 @@ class MPscanner {
     	return true;
     }
     
-    public void getToken() {
+    public Token getToken() throws IOException {
+    	//Token token = new Token();
+    	int ch = reader.read();
     	
+    	if(ch < 0) {
+    		return new Token(Token.TokenName.MP_EOF, lineNumber, columnNumber, String.valueOf(ch) );
+    	}
+    	char nextChar = (char) ch;
+    	
+    	
+    	return token;
     }
     
     public void getLexeme() {
