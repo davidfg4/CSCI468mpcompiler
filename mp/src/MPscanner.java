d53632e10fb6fe123a0f0c810a2a9661796fde7e
David Gianforte <david.gianforte@gmail.com>
2013-01-22 21:01:39 -0700

    scan numbers

--- mp/src/MPscanner.java
+++ mp/src/MPscanner.java
@@ -112,119 +112,189 @@ class MPscanner {
     		return findIdentifier();
     	}
     	else if (isNumber(ch)) {
-    		return null;
+    		return findInteger();
     	}
     	else {
     		return returnToken(Token.TokenName.MP_ERROR);
     	}
     }
     
-    private Token findIdentifier() throws IOException {
-    	markBuffer();
-    	char ch = getNextChar();
-    	if (ch == '_') {
-    		ch = getNextChar();
-    		if (isLetter(ch) || isNumber(ch)) {
-    			return findIdentifier();
-    		}
-    	}
-    	else if (isLetter(ch) || isNumber(ch)) {
-    		return findIdentifier();
-    	}
-    	resetBuffer();
-    	Token.TokenName reservedWord = Token.getReservedWord(lexeme.toString());
-    	if (reservedWord != null) {
-    		return returnToken(reservedWord);
-    	}
-    	else {
-    		return returnToken(Token.TokenName.MP_IDENTIFIER);
-    	}
-    }
-    
-    private boolean isLetter(char c) {
-    	if(c >= 'a' && c <= 'z' ||
-    			c >= 'A' && c <= 'Z') {
-    		return true;
-    	}
-    	return false;
-    }
-    
-    private boolean isNumber(char c) {
-    	if(c >= '0' && c <= '9') {
-    		return true;
-    	}
-    	return false;
-    }
-    
-    private char getNextChar() throws IOException {
-    	int ch2 = reader.read();
-    	if (ch2 == -1) {
-    		ch2 = 4;
-    	}
-    	char ch = (char) ch2;
-    	lexeme.append(ch);
-    	columnNumber++;
-    	if((char)ch == '\n') {
-    		lineNumber++;
-    		columnNumber = 1;
-    	}
-    	return ch;
-    }
-    
-    private void markBuffer() {
-    	markedLexeme = lexeme.toString();
-    	markedLine = lineNumber;
-    	markedColumn = columnNumber;
-    	try {
+	private Token findIdentifier() throws IOException {
+		markBuffer();
+		char ch = getNextChar();
+		if (ch == '_') {
+			ch = getNextChar();
+			if (isLetter(ch) || isNumber(ch)) {
+				return findIdentifier();
+			}
+		}
+		else if (isLetter(ch) || isNumber(ch)) {
+			return findIdentifier();
+		}
+		resetBuffer();
+		Token.TokenName reservedWord = Token.getReservedWord(lexeme.toString());
+		if (reservedWord != null) {
+			return returnToken(reservedWord);
+		}
+		else {
+			return returnToken(Token.TokenName.MP_IDENTIFIER);
+		}
+	}
+	
+	private Token findInteger() throws IOException {
+		markBuffer();
+		char ch = getNextChar();
+		if(isNumber(ch)) {
+			return findInteger();
+		}
+		else if (ch == '.') {
+			ch = getNextChar();
+			if (isNumber(ch)) {
+				return findFixed();
+			}
+			else {
+				resetBuffer();
+				return returnToken(Token.TokenName.MP_INTEGER);
+			}
+		}
+		else if (ch == 'e' || ch == 'E') {
+			ch = getNextChar();
+			if (ch == '+' || ch == '-') {
+				ch = getNextChar();
+			}
+			if (isNumber(ch)) {
+				return findFloat();
+			}
+			else {
+				return returnToken(Token.TokenName.MP_INTEGER);
+			}
+		}
+		else {
+			resetBuffer();
+			return returnToken(Token.TokenName.MP_INTEGER);
+		}
+	}
+	
+	private Token findFixed() throws IOException {
+		markBuffer();
+		char ch = getNextChar();
+		if(isNumber(ch)) {
+			return findFixed();
+		}
+		else if (ch == 'e' || ch == 'E') {
+			ch = getNextChar();
+			if (ch == '+' || ch == '-') {
+				ch = getNextChar();
+			}
+			if (isNumber(ch)) {
+				return findFloat();
+			}
+			else {
+				return returnToken(Token.TokenName.MP_FIXED);
+			}
+		}
+		else {
+			resetBuffer();
+			return returnToken(Token.TokenName.MP_FIXED);
+		}
+	}
+	
+	private Token findFloat() throws IOException {
+		markBuffer();
+		char ch = getNextChar();
+		if(isNumber(ch)) {
+			return findFloat();
+		}
+		else {
+			resetBuffer();
+			return returnToken(Token.TokenName.MP_FLOAT);
+		}
+	}
+	
+	private boolean isLetter(char c) {
+		if(c >= 'a' && c <= 'z' ||
+				c >= 'A' && c <= 'Z') {
+			return true;
+		}
+		return false;
+	}
+	
+	private boolean isNumber(char c) {
+		if(c >= '0' && c <= '9') {
+			return true;
+		}
+		return false;
+	}
+	
+	private char getNextChar() throws IOException {
+		int ch2 = reader.read();
+		if (ch2 == -1) {
+			ch2 = 4;
+		}
+		char ch = (char) ch2;
+		lexeme.append(ch);
+		columnNumber++;
+		if((char)ch == '\n') {
+			lineNumber++;
+			columnNumber = 1;
+		}
+		return ch;
+	}
+	
+	private void markBuffer() {
+		markedLexeme = lexeme.toString();
+		markedLine = lineNumber;
+		markedColumn = columnNumber;
+		try {
 			reader.mark(10);
 		} catch (IOException e) {
 			System.out.println("Error: Failed to mark reader");
 			System.exit(1);
 		}
-    }
-    
-    private void resetBuffer() {
-    	lexeme = new StringBuilder(markedLexeme);
-    	lineNumber = markedLine;
-    	columnNumber = markedColumn;
-    	try {
+	}
+	
+	private void resetBuffer() {
+		lexeme = new StringBuilder(markedLexeme);
+		lineNumber = markedLine;
+		columnNumber = markedColumn;
+		try {
 			reader.reset();
 		} catch (IOException e) {
 			System.out.println("Error: Failed to reset marker");
 			System.exit(1);
 		}
-    }
-    
-    private Token returnToken(Token.TokenName tokenName) {
-    	token = new Token(tokenName, lineNumber, tokenStartColumn,
-    			lexeme.toString());
-    	return token;
-    }
-    
-    public String getLexeme() {
-    	if (token == null) {
-    		return "";
-    	}
-    	else {
-    		return token.getLexeme();
-    	}
-    }
-    
-    public int getLineNumber() {
-    	if (token == null) {
-    		return 0;
-    	}
-    	else {
-    		return token.getLine();
-    	}
-    }
-    
-    public int getColumnNumber() {
-    	if (token == null) {
-    		return 0;
-    	}
-    	else {
-    		return token.getColumn();
-    	}
-    }
+	}
+	
+	private Token returnToken(Token.TokenName tokenName) {
+		token = new Token(tokenName, lineNumber, tokenStartColumn,
+				lexeme.toString());
+		return token;
+	}
+	
+	public String getLexeme() {
+		if (token == null) {
+			return "";
+		}
+		else {
+			return token.getLexeme();
+		}
+	}
+	
+	public int getLineNumber() {
+		if (token == null) {
+			return 0;
+		}
+		else {
+			return token.getLine();
+		}
+	}
+	
+	public int getColumnNumber() {
+		if (token == null) {
+			return 0;
+		}
+		else {
+			return token.getColumn();
+		}
+	}
 }
