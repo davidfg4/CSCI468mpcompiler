ff5b9176fa837ae20eab7b9036eb6d5db3cc5eec
David Gianforte <david.gianforte@gmail.com>
2013-02-25 19:19:00 -0700

    support creating and deleting symbol tables

--- mp/src/symbolTable.java
+++ mp/src/symbolTable.java
@@ -1,3 +1,29 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+
 public class symbolTable {
+	private LinkedList<HashSet<symbol>> symbolTables;
 	
+	symbolTable() {
+		symbolTables = new LinkedList<HashSet<symbol>>();
+	}
+	
+	public void insertSymbol(symbol s) {
+		
+	}
+	
+	public void findSymbol(String name) {
+		
+	}
+	
+	public void createSymbolTable() {
+		symbolTables.add(0, new HashSet<symbol>());
+	}
+	
+	public boolean deleteSymbolTable() {
+		if (symbolTables.remove() == null)
+			return false;
+		else
+			return true;
+	}
 }
